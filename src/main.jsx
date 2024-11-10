55822c3fbe72d98e3e7918d17ee74cf8d117a818
TanzidAnan <tanzid.anan125@gmail.com>
2024-11-11 01:09:17 +0600

    Navber section

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,8 @@ import {
 } from "react-router-dom";
 import MainLayout from './Layouts/MainLayout.jsx';
 import Home from './Compontents/Home/Home.jsx';
+import Login from './Compontents/Login/Login.jsx';
+import Register from './Compontents/Register/Register.jsx';
 
 const router = createBrowserRouter([
   {
@@ -18,9 +20,18 @@ const router = createBrowserRouter([
       {
         path:'/',
         element:<Home></Home>
+      },
+      {
+        path:'/login',
+        element:<Login></Login>
+      },
+      {
+        path:'/register',
+        element:<Register></Register>
       }
     ]
   },
+  
 ]);
 
 createRoot(document.getElementById('root')).render(
