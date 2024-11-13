56fdac0adb4bcc6afc3b3b561a85c0ee6e78c561
TanzidAnan <tanzid.anan125@gmail.com>
2024-11-13 20:04:37 +0600

    login

--- src/Compontents/Login/Login.jsx
+++ src/Compontents/Login/Login.jsx
@@ -1,8 +1,69 @@
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase.config";
+import { useState } from "react";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Login = () => {
+    const [success,setSuccess] =useState(false)
+
+    const hendleLogin =(e) =>{
+        e.preventDefault();
+        const email =e.target.email.value;
+        const password =e.target.password.value;
+        console.log(email,password)
+        setSuccess(false)
+
+        signInWithEmailAndPassword(auth,email,password)
+        .then(res =>{
+            console.log(res.user)
+            setSuccess(true)
+            toast('success full login')
+        })
+        .catch(error =>{
+            console.log(error.message)
+            toast.error('no login')
+        })
+    }
+
     return (
-        <div>
-            <h1>This is Login</h1>
+        <div className="hero bg-base-200 min-h-screen">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <div className="text-center lg:text-left">
+                    <h1 className="text-5xl font-bold">Login now!</h1>
+                    <p className="py-6">
+                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+                        quasi. In deleniti eaque aut repudiandae et a id nisi.
+                    </p>
+                </div>
+                <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                    <form onSubmit={hendleLogin} className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" name="email" placeholder="email" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" name="password" placeholder="password" className="input input-bordered" required />
+                            <label className="label">
+                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">Login</button>
+                        </div>
+                    </form>
+                    {
+                        success && <p>Login success full</p>
+                        
+                    }
+                </div>
+                <ToastContainer />
+            </div>
         </div>
     );
 };
