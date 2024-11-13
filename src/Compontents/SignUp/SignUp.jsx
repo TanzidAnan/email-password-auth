5259849b52a587b662cdee5cfc13c3ab2c23545d
TanzidAnan <tanzid.anan125@gmail.com>
2024-11-13 15:13:08 +0600

    sign up login

--- src/Compontents/SignUp/SignUp.jsx
+++ src/Compontents/SignUp/SignUp.jsx
@@ -16,7 +16,7 @@ const SignUp = () => {
                             <path
                                 d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
                         </svg>
-                        <input type="email" name='email' className="grow" placeholder="Email" />
+                        <input type="email" name='email' className="grow" placeholder="Email" required />
                     </label>
                     <label className="input input-bordered flex items-center gap-2 mt-7">
                         <svg
@@ -29,7 +29,10 @@ const SignUp = () => {
                                 d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
                                 clipRule="evenodd" />
                         </svg>
-                        <input type="password" name='password' className="grow" placeholder='password' />
+                        <input type="password" name='password' className="grow" placeholder='password' required />
+                    </label>
+                    <label className="label">
+                        <a href="#" className="label-text-alt link link-hover text-black hover:text-red-900">Forgot password?</a>
                     </label>
                 </div>
                 <div className='mt-7'>
