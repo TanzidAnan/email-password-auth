3827921293a4ff43894ec2a7464e4bf9dd03c349
TanzidAnan <tanzid.anan125@gmail.com>
2024-12-09 21:20:58 +0600

    home page

--- src/Compontents/Home/Home.jsx
+++ src/Compontents/Home/Home.jsx
@@ -2,7 +2,20 @@
 const Home = () => {
     return (
         <div>
-            <h1 className="text-2xl">This is Home password</h1>
+            <section>
+                <div className="hero bg-base-200 min-h-[60vh]">
+                    <div className="hero-content text-center">
+                        <div className="max-w-md">
+                            <h1 className="text-5xl font-bold">Hello there</h1>
+                            <p className="py-6">
+                                Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+                                quasi. In deleniti eaque aut repudiandae et a id nisi.
+                            </p>
+                            <button className="btn btn-primary">Get Started</button>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </div>
     );
 };
