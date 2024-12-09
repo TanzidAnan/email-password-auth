d5aac3a2d5af9c3ba9d1a73d9ccb9cead7710f1d
TanzidAnan <tanzid.anan125@gmail.com>
2024-12-09 21:26:00 +0600

    home section

--- src/Compontents/Home/Home.jsx
+++ src/Compontents/Home/Home.jsx
@@ -3,11 +3,16 @@ const Home = () => {
     return (
         <div>
             <section>
-                <div className="hero bg-base-200 min-h-[60vh]">
-                    <div className="hero-content text-center">
+                <div
+                    className="hero min-h-[70vh]"
+                    style={{
+                        backgroundImage: "url(https://i.ibb.co.com/Rgkcsjb/the-poster-of-thalapathy-vijays-68th-film-313712690-16x9-0.jpg)",
+                    }}>
+                    <div className="hero-overlay bg-opacity-60"></div>
+                    <div className="hero-content text-neutral-content text-center">
                         <div className="max-w-md">
-                            <h1 className="text-5xl font-bold">Hello there</h1>
-                            <p className="py-6">
+                            <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+                            <p className="mb-5">
                                 Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
                                 quasi. In deleniti eaque aut repudiandae et a id nisi.
                             </p>
