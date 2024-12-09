4b75e29e9f673f3a06535d94fa52a1884c8409be
TanzidAnan <tanzid.anan125@gmail.com>
2024-12-09 21:28:32 +0600

    home page

--- src/Compontents/Home/Home.jsx
+++ src/Compontents/Home/Home.jsx
@@ -6,9 +6,9 @@ const Home = () => {
                 <div
                     className="hero min-h-[70vh]"
                     style={{
-                        backgroundImage: "url(https://i.ibb.co.com/Rgkcsjb/the-poster-of-thalapathy-vijays-68th-film-313712690-16x9-0.jpg)",
+                        backgroundImage: "url(https://i.ibb.co.com/WFd0kKC/18-Waterpark-MS-9635.jpg)",
                     }}>
-                    <div className="hero-overlay bg-opacity-60"></div>
+                    <div className="hero-overlay bg-opacity-10"></div>
                     <div className="hero-content text-neutral-content text-center">
                         <div className="max-w-md">
                             <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
